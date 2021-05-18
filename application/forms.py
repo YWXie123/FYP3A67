44eb3e6631f720c56ad660b74940fb29499c5b1f
weiwen01 <weiwen.19@ichat.sp.edu.sg>
2021-05-18 15:59:28 +0800

    added models.py for database

--- application/forms.py
+++ application/forms.py
@@ -3,36 +3,11 @@ from wtforms import FloatField, SubmitField, StringField, PasswordField
 from wtforms.validators import Length, InputRequired, ValidationError, NumberRange
 
 class PredictionForm(FlaskForm):
-    age   = FloatField("Age", 
-               validators=[InputRequired(), NumberRange(0,200)])
-    sex   = FloatField("Sex",
-               validators=[InputRequired(), NumberRange(0,1)])
-    cp   = FloatField("Chest Pain Type(cp)",
-               validators=[InputRequired(), NumberRange(0,3)])
-    trestbps   = FloatField("Resting blood pressure(trestbps)",
-               validators=[InputRequired(), NumberRange(0,300)])
-    chol   = FloatField("Serum Cholestoral(chol)",
-               validators=[InputRequired(), NumberRange(0,1000)])
-    fbs   = FloatField("Fasting Blood Sugar > 120 mg/dl(fbs)",
-               validators=[InputRequired(), NumberRange(0,1)])
-    restecg   = FloatField("Resting Electrocardiographic Results(restecg)",
-               validators=[InputRequired(), NumberRange(0,2)])
-    thalach   = FloatField("Maximum Heart Rate Achieved(thalach)",
-               validators=[InputRequired(), NumberRange(0,300)])
-    exang   = FloatField("Exercise Induced Angina(exang)",
-               validators=[InputRequired(), NumberRange(0,1)])
-    oldpeak   = FloatField("ST Depression Induced By Exercise Relative To Rest(oldpeak)",
-               validators=[InputRequired(), NumberRange(0,10)])
-    slope   = FloatField("Slope Of The Peak Exercise ST Segment(slope)",
-               validators=[InputRequired(), NumberRange(0,2)])
-    ca   = FloatField("Number Of Major Vessels Colored By Flourosopy(ca)",
-               validators=[InputRequired(), NumberRange(0,4)])
-    thal   = FloatField("Normal, Fixed Defect, Reversable Defect(thal)",
-               validators=[InputRequired(), NumberRange(0,3)])
+    question = StringField("Question here",
+                validators = [InputRequired()])
     submit = SubmitField("Predict")
 
 class SignUpForm(FlaskForm):
-    email   = StringField("Email", validators=[InputRequired()])
     password   = PasswordField("Password", validators=[InputRequired()])
     name   = StringField("Name", validators=[InputRequired()])
     submit = SubmitField("Submit")
---
+++ application/models.py
@@ -0,0 +1,32 @@
+from application import db
+import datetime as dt
+from sqlalchemy.orm import backref, validates
+ 
+class Staff(db.Model):
+    __tablename__ = 'staff'
+ 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    staffid = db.relationship('Staff Id',backref = 'staff', lazy=True)
+    name = db.Column(db.String(20))
+    position = db.Column(db.String(80))
+    department = db.Column(db.String(20))
+
+class SearchHist(db.Model):
+    __tablename__ = 'search_history'
+ 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    staffid = db.Column(db.Integer, db.ForeignKey('staff.id'), nullable = False)
+    askedon = db.Column(db.DateTime, nullable = False)
+    question = db.Column(db.String(200))
+
+class Answer(db.Model):
+    __tablename__ = 'answer'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    answer = db.Column(db.String(300))
+
+class Question(db.Model):
+    __tablename__ = 'question'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    answer = db.Column(db.String(300))
